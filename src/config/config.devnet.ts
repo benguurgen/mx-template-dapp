742be303f0dfe9594bbd76ff32cf72501e0dfb7f
Bengü Ürgen Demir <benguurgen@gmail.com>
2025-02-07 16:55:20 +0300

    -yarn change

--- src/config/config.devnet.ts
+++ src/config/config.devnet.ts
@@ -3,7 +3,7 @@ import { EnvironmentsEnum } from 'types';
 export * from './sharedConfig';
 
 export const contractAddress =
-  'erd1qqqqqqqqqqqqqpgqm6ad6xrsjvxlcdcffqe8w58trpec09ug9l5qde96pq';
+  'erd1pf3cjs35c9vc4hp84lc8308qxg6vt2w8gj8zte858un5ta5mshzsgnsrx5';
 export const API_URL = 'https://devnet-template-api.multiversx.com';
 export const sampleAuthenticatedDomains = [API_URL];
 export const environment = EnvironmentsEnum.devnet;
